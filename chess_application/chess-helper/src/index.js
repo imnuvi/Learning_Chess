ea6bde37ef7f059630314b5dc137ee2a51bbce0a
cosmoglint <ramprakashsmail@gmail.com>
2021-01-19 21:24:20 +0530

    ADD:  Board component

--- chess_application/chess-helper/src/index.js
+++ chess_application/chess-helper/src/index.js
@@ -1,11 +1,32 @@
+import './style.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+class Square extends React.Component {
+  render(){
+    return(
+      <div className='square'>
+      </div>
+    );
+  }
+}
+
+class Board extends React.Component {
+  render(){
+    return(
+      <div>
+        <div>This is status</div>
+      </div>
+    )
+  }
+}
 
 class Game extends React.Component {
   render() {
     return(
       <div className='Game'>
         <p>Hello world !</p>
+        <Board />
       </div>
     );
   }
