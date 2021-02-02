fc7bd8a7074e9e33edc99255fd3b2a293ac89cb1
ram prakash <ramprakashsmail@gmail.com>
2021-02-02 20:34:48 +0530

    ADD:  correct logic for rookh

--- chess_application/chess-helper/src/logic.js
+++ chess_application/chess-helper/src/logic.js
@@ -77,58 +77,64 @@ function rookhMoves(position,piece,board){
   let bottom_branch = true;
   let left_branch = true;
   let right_branch = true;
-
   let friend = piece.charAt(0);
   let enemy = (friend === 'b') ? 'w' : 'b';
 
 
-  for (let i=1; i<7; i++){
+  for (let i=1; i<=7; i++){
     if (oldx+i <= 7 && bottom_branch){
        if (board[oldx+i][oldy] === null){
          rookh_list.push([oldx+i,oldy]);
-         continue
        }
-       if (board[oldx+i][oldy].charAt(0) === enemy){
+       else if (board[oldx+i][oldy].charAt(0) === enemy){
          rookh_list.push([oldx+i,oldy]);
          bottom_branch = false;
-         continue
+       }
+       else if (board[oldx+i][oldy].charAt(0) === friend){
+         bottom_branch = false;
        }
     }
+
     if (oldx-i >= 0 && top_branch){
       if (board[oldx-i][oldy] === null){
         rookh_list.push([oldx-i,oldy]);
-        continue
       }
-      if (board[oldx-i][oldy].charAt(0) === enemy){
+      else if (board[oldx-i][oldy].charAt(0) === enemy){
         rookh_list.push([oldx-i,oldy]);
         top_branch = false;
-        continue
+      }
+      else if (board[oldx-i][oldy].charAt(0) === friend){
+        top_branch = false;
       }
     }
+
     if (oldy+i <= 7 && right_branch){
       if (board[oldx][oldy+i] === null){
-        rookh_list.push([oldx-i,oldy]);
-        continue
+        rookh_list.push([oldx,oldy+i]);
       }
-      if (board[oldx][oldy+i].charAt(0) === enemy){
+      else if (board[oldx][oldy+i].charAt(0) === enemy){
         rookh_list.push([oldx,oldy+i]);
         right_branch = false;
-        continue
+      }
+      else if (board[oldx][oldy+i].charAt(0) === friend){
+        right_branch = false;
       }
     }
+
     if (oldy-i >= 0 && left_branch){
       if (board[oldx][oldy-i] === null){
-        rookh_list.push([oldx-i,oldy]);
-        continue
+        rookh_list.push([oldx,oldy-i]);
       }
-      if (board[oldx][oldy-i].charAt(0) === enemy){
+      else if (board[oldx][oldy-i].charAt(0) === enemy){
         rookh_list.push([oldx,oldy-i]);
         left_branch = false;
-        continue
+      }
+      else if (board[oldx][oldy-i].charAt(0) === friend){
+        left_branch = false;
       }
     }
   }
-
+  return (rookh_list);
 }
 
 // function validMoves(possible_moves,position){
@@ -158,6 +164,9 @@ function move(start,end,piece,board){
   }
   if (piece === 'wpa' || piece === 'bpa'){
     console.log(pawnMoves(start,piece,board));
+  }
+  if (piece === 'wro' || piece === 'bro'){
+    console.log(rookhMoves(start,piece,board));
   }
 
   // let startx,starty;
